c200c3c2eb2322f031cf3802d4b8dc321b5344dc
musicpresale <delapannoer@gmail.com>
2025-08-17 02:09:40 +0700

    Update transactions.js

--- api/transactions.js
+++ api/transactions.js
@@ -1,25 +1,41 @@
-// api/transactions.js
-let data = {
-  price: 0.0075,
-  circulatingSupply: 100000000,
-  tradeVolume: 0,
-  userUSD: 1000,
-  userBDC: 0,
-  log: []
-};
+import { kv } from '@vercel/kv';
 
-export default function handler(req, res) {
-  if (req.method === "GET") {
+const DATA_KEY = 'simulasi-bdc-data';
+
+// Inisialisasi data default
+async function getData() {
+  let data = await kv.get(DATA_KEY);
+  if (!data) {
+    data = {
+      price: 0.0075,
+      circulatingSupply: 100000000,
+      tradeVolume: 0,
+      userUSD: 1000,
+      userBDC: 0,
+      log: []
+    };
+    await kv.set(DATA_KEY, data);
+  }
+  return data;
+}
+
+export default async function handler(req, res) {
+  let data = await getData();
+
+  if (req.method === 'GET') {
     res.status(200).json(data);
-  } else if (req.method === "POST") {
+    return;
+  }
+
+  if (req.method === 'POST') {
     const { type, usd, koin, transaksi } = req.body;
 
-    if (type === "buy") {
+    if (type === 'buy') {
       data.userUSD -= usd;
       data.userBDC += koin;
       data.circulatingSupply += koin;
       data.tradeVolume += usd;
-    } else if (type === "sell") {
+    } else if (type === 'sell') {
       data.userUSD += usd;
       data.userBDC -= koin;
       data.circulatingSupply -= koin;
@@ -27,9 +43,12 @@ export default function handler(req, res) {
     }
 
     data.log.unshift({ ...transaksi, waktu: new Date() });
-    if (data.log.length > 100) data.log.pop(); // batasi log 100
+    if (data.log.length > 100) data.log.pop(); // batasi log
+
+    await kv.set(DATA_KEY, data);
     res.status(200).json(data);
-  } else {
-    res.status(405).send("Method Not Allowed");
+    return;
   }
+
+  res.status(405).send('Method Not Allowed');
 }
